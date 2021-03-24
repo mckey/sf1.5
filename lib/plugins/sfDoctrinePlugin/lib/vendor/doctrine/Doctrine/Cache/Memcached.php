19f07f37820614f079671d2c86ab09c5c6d0904d
Vladimir Makarov <mckey11@gmail.com>
2021-03-24 18:09:15 +0200

    Small fixes for update tasks. (memcached flush)
    Signed-off-by: Vladimir Makarov <mckey11@gmail.com>

--- lib/plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Cache/Memcached.php
+++ lib/plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Cache/Memcached.php
@@ -15,28 +15,35 @@
 class Doctrine_Cache_Memcached extends Doctrine_Cache_Driver
 {
     /**
-     * @var Memcached $_memcached memcached object
+     * @var Memcached|null $_memcached memcached object
      */
-    protected $_memcached = null;
+    protected ?Memcached $_memcached = null;
 
     /**
      * constructor
      *
      * @param array $options associative array of cache driver options
+     *
+     * @throws Doctrine_Cache_Exception
      */
     public function __construct($options = [])
     {
         if (!extension_loaded('memcached')) {
-            throw new Doctrine_Cache_Exception('In order to use Memcached driver, the memcached extension must be loaded.');
+            throw new Doctrine_Cache_Exception(
+                'In order to use Memcached driver, the memcached extension must be loaded.'
+            );
         }
+
         parent::__construct($options);
 
         if (isset($options['servers'])) {
             $value = $options['servers'];
+
             if (isset($value['host'])) {
                 // in this case, $value seems to be a simple associative array (one server only)
                 $value = [0 => $value]; // let's transform it into a classical array of associative arrays
             }
+
             $this->setOption('servers', $value);
         }
 
@@ -48,12 +55,15 @@ class Doctrine_Cache_Memcached extends Doctrine_Cache_Driver
             }
 
             $active_servers = $this->_memcached->getServerList();
-            if (is_array($active_servers)) {
+
+            if (is_array($active_servers) && count($active_servers)) {
                 foreach ($active_servers as $active_server) {
                     if ($active_server['host'] != $server['host'] || $active_server['port'] != $server['port']) {
                         $this->_memcached->addServer($server['host'], $server['port']);
                     }
                 }
+            } else {
+                $this->_memcached->addServer($server['host'], $server['port']);
             }
         }
     }
@@ -62,6 +72,7 @@ class Doctrine_Cache_Memcached extends Doctrine_Cache_Driver
      * Test if a cache record exists for the passed id
      *
      * @param string $id cache id
+     *
      * @return mixed  Returns either the cached data or false
      */
     protected function _doFetch($id, $testCacheValidity = true)
@@ -73,9 +84,10 @@ class Doctrine_Cache_Memcached extends Doctrine_Cache_Driver
      * Test if a cache is available or not (for the given id)
      *
      * @param string $id cache id
+     *
      * @return mixed false (a cache is not available) or "last modified" timestamp (int) of the available cache record
      */
-    protected function _doContains($id)
+    protected function _doContains($id) : bool
     {
         return (bool)$this->_memcached->get($id);
     }
@@ -84,12 +96,13 @@ class Doctrine_Cache_Memcached extends Doctrine_Cache_Driver
      * Save a cache record directly. This method is implemented by the cache
      * drivers and used in Doctrine_Cache_Driver::save()
      *
-     * @param string $id cache id
-     * @param string $data data to cache
+     * @param string    $id       cache id
+     * @param string    $data     data to cache
      * @param int|false $lifeTime if != false, set a specific lifetime for this cache record (null => infinite lifeTime)
-     * @return boolean true if no problem
+     *
+     * @return bool true if no problem
      */
-    protected function _doSave($id, $data, $lifeTime = false)
+    protected function _doSave($id, $data, $lifeTime = false) : bool
     {
         if ($lifeTime === false) {
             $lifeTime = 0;
@@ -103,9 +116,10 @@ class Doctrine_Cache_Memcached extends Doctrine_Cache_Driver
      * drivers and used in Doctrine_Cache_Driver::delete()
      *
      * @param string $id cache id
-     * @return boolean true if no problem
+     *
+     * @return bool true if no problem
      */
-    protected function _doDelete($id)
+    protected function _doDelete($id) : bool
     {
         return $this->_memcached->delete($id);
     }
@@ -113,7 +127,7 @@ class Doctrine_Cache_Memcached extends Doctrine_Cache_Driver
     /**
      * Fetch an array of all keys stored in cache
      *
-     * @return array Returns the array of cache keys
+     * @return array|false Returns the array of cache keys
      */
     protected function _getCacheKeys()
     {
